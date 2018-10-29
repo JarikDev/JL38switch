80b6b19c29733dd58f65dce0a9dbfa8f66c89b59
ivan4000 <sxln1@mail.ru>
2018-10-29 14:05:03 +0300

    JL38 last commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+import java.time.DayOfWeek;
+
 public class Main {
+    enum Days {MONDAY, THURSDAY, WEDNESDAY}
+
     public static void main(String[] args) {
-        int i=5;
-        switch (i){}
+        int i = 5;
+        switch (i) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 5:
+                System.out.println("5");
+                break;
+            case 6:
+                System.out.println("6");
+                break;
+            default:
+                System.out.println("default");
+                break;
+
+        }
+        String s = "10";
+        switch (s) {
+            case "1":
+                System.out.println("1");
+            case "2":
+                System.out.println("2");
+            default:
+                System.out.println("default");
+            case "3":
+                System.out.println("3");
+            case "5":
+                System.out.println("5");
+            case "6":
+                System.out.println("6");
+
+        }
+
+
     }
 }
